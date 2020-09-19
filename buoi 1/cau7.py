5b5c13c322833fb4ba5a5986dbe69c098b7cd874
Thoaitran260901 <71496661+Thoaitran260901@users.noreply.github.com>
2020-09-19 18:06:45 +0700

    Update cau7.py

--- buoi 1/cau7.py
+++ buoi 1/cau7.py
@@ -1,7 +1,4 @@
 print("----HÌNH TAM GIÁC----")
-mang = int(input("nhap so luong: "))
-for cdoc in range(mang):
- for cngang in range(mang):
-     if cngang == range(mang) or cngang < cdoc or cngang == cdoc :
-      print("*",end="")
- print()
+print("     *       ")
+print("    ***      ")
+print("   *****     ")
