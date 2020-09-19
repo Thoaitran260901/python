12d867c727244d02474e6252f0a43e9665e59ba0
Thoaitran260901 <71496661+Thoaitran260901@users.noreply.github.com>
2020-09-19 18:06:10 +0700

    Update cau5.py

--- buoi 1/cau5.py
+++ buoi 1/cau5.py
@@ -1,7 +1,4 @@
 print("----HÌNH CHỮ NHẬT----")
-chieudai = int(input("nhap chieu dai: "))
-chieurong = int(input("nhap chieu rong: "))
-for cd in range(chieudai):
-    for cr in range(chieurong):
-        print("*", end="")
-    print()
+print("***********")
+print("***********")
+print("***********")
