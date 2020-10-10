4df80177a5bedf16f25aeade7d577d823493fc33
Thoaitran260901 <71496661+Thoaitran260901@users.noreply.github.com>
2020-10-10 16:54:53 +0700

    Update cau5.py

--- Buoi 4/cau5.py
+++ Buoi 4/cau5.py
@@ -1,4 +1,11 @@
 chuoi=input("Nhập chuỗi: ")
-def max_min(chuoi):
-    return chuoi[:1],chuoi[-1:]
-print(max_min(chuoi))
+def minmaxchuoi(chuoi):
+    max=min=chuoi[0]
+    for i in chuoi:
+        if min>i:
+            min=i
+        if max<i:
+            max=i
+    print("Ki tu lon nhat la",max)
+    print("ki tu nho nhat la",min)
+minmaxchuoi(chuoi)
